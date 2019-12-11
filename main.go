ec2b0d78f42c2a79d4baed34221caba6153d1ae2
AnchalJain <anchalj109@gmail.com>
2019-12-11 23:06:26 +0530

    updated env vairables

--- main.go
+++ main.go
@@ -1,45 +1,46 @@
 package main
 
 import (
-
 	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"text/template"
-	_"github.com/go-sql-driver/mysql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Todo struct {
-	Id int
-	Title string
+	Id          int
+	Title       string
 	Description string
 }
 
 func dbConn() (db *sql.DB) {
-	dbDriver:="mysql"
-	dbUser:="root"
-	dbPass:="forceofnature"
-	dbName:="goblog"
+	dbDriver := "mysql"
+	dbUser := os.Getenv("DBUSER")
+	dbPass := os.Getenv("DBPASS")
+	dbName := "todo"
 
-	db, err:=sql.Open(dbDriver,dbUser+":"+dbPass+"@/"+dbName)
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
 	if err != nil {
 		panic(err.Error())
 	}
 	return db
 }
 
-var tmpl=template.Must(template.ParseGlob("form/*"))
+var tmpl = template.Must(template.ParseGlob("form/*"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	db:=dbConn()
-	selDb, err:=db.Query("SELECT * FROM todo ORDER BY id DESC")
+	db := dbConn()
+	selDb, err := db.Query("SELECT * FROM todo ORDER BY id DESC")
 
-	if err!= nil {
+	if err != nil {
 		panic(err.Error())
 	}
 
-	todo:=Todo{}
-	res:=[]Todo{}
+	todo := Todo{}
+	res := []Todo{}
 	for selDb.Next() {
 		var id int
 		var title, description string
@@ -47,10 +48,10 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			panic(err.Error())
 		}
-		todo.Id=id
-		todo.Title=title
-		todo.Description=description
-		res=append(res, todo)
+		todo.Id = id
+		todo.Title = title
+		todo.Description = description
+		res = append(res, todo)
 	}
 	w.Header().Set("Content-Type", "text/html")
 	tmpl.ExecuteTemplate(w, "Index", res)
@@ -92,23 +93,23 @@ func New(w http.ResponseWriter, r *http.Request) {
 }
 
 func Edit(w http.ResponseWriter, r *http.Request) {
-	db:=dbConn()
-	nId:=r.URL.Query().Get("id")
-	selDb, err:=db.Query("SELECT * FROM todo WHERE id=?", nId)
+	db := dbConn()
+	nId := r.URL.Query().Get("id")
+	selDb, err := db.Query("SELECT * FROM todo WHERE id=?", nId)
 	if err != nil {
 		panic(err.Error())
 	}
-	todo:=Todo{}
+	todo := Todo{}
 	for selDb.Next() {
 		var id int
 		var title, description string
-		err=selDb.Scan(&id, &title, &description)
+		err = selDb.Scan(&id, &title, &description)
 		if err != nil {
 			panic(err.Error())
 		}
-		todo.Id=id
-		todo.Title=title
-		todo.Description=description
+		todo.Id = id
+		todo.Title = title
+		todo.Description = description
 	}
 	w.Header().Set("Content-Type", "text/html")
 	tmpl.ExecuteTemplate(w, "Edit", todo)
@@ -116,7 +117,7 @@ func Edit(w http.ResponseWriter, r *http.Request) {
 }
 
 func Insert(w http.ResponseWriter, r *http.Request) {
-	db:=dbConn()
+	db := dbConn()
 	if r.Method == "POST" {
 		title := r.FormValue("title")
 		description := r.FormValue("description")
@@ -125,7 +126,7 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 			panic(err.Error())
 		}
 		insForm.Exec(title, description)
-//		log.Println("INSERT: Title: "+ title + "Description: "+ description)
+		//		log.Println("INSERT: Title: "+ title + "Description: "+ description)
 	}
 	defer db.Close()
 	http.Redirect(w, r, "/", 301)
@@ -134,38 +135,38 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 func Update(w http.ResponseWriter, r *http.Request) {
 	db := dbConn()
 	if r.Method == "POST" {
-	title:=r.FormValue("title")
-	description:=r.FormValue("description")
-	id:=r.FormValue("uid")
-	insForm, err := db.Prepare("UPDATE todo SET title=?, description=? WHERE id=?")
-	if err != nil {
-		panic (err.Error())
-	}
-	insForm.Exec(title, description, id)
-	log.Println("UPDATE: Title: "+ title + " | Description: " + description)
+		title := r.FormValue("title")
+		description := r.FormValue("description")
+		id := r.FormValue("uid")
+		insForm, err := db.Prepare("UPDATE todo SET title=?, description=? WHERE id=?")
+		if err != nil {
+			panic(err.Error())
+		}
+		insForm.Exec(title, description, id)
+		log.Println("UPDATE: Title: " + title + " | Description: " + description)
 	}
 	defer db.Close()
 	http.Redirect(w, r, "/", 301)
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
-    db := dbConn()
-    todo:=r.URL.Query().Get("id")
-    log.Println(todo)
-    delForm, err := db.Prepare("DELETE FROM todo WHERE id=?")
-    if err != nil {
-        panic(err.Error())
-    }
-    log.Println("DELETE")
-    delForm.Exec(todo)
-    defer db.Close()
-    http.Redirect(w, r, "/", 301)
+	db := dbConn()
+	todo := r.URL.Query().Get("id")
+	log.Println(todo)
+	delForm, err := db.Prepare("DELETE FROM todo WHERE id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+	log.Println("DELETE")
+	delForm.Exec(todo)
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
 }
 
 func main() {
 	log.Println("Server started on : http://localhost:8080")
 	http.HandleFunc("/", Index)
-//	http.HandleFunc("/show", Show)
+	//	http.HandleFunc("/show", Show)
 	http.HandleFunc("/new", New)
 	http.HandleFunc("/edit", Edit)
 	http.HandleFunc("/insert", Insert)
